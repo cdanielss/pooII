42ccd09393c05206fea6781c7ae720cd7eae7a4d
Cicero777 <cicero_neto13@hotmail.com>
2021-04-26 19:59:41 -0300

    Classe 'banco' adicionada

--- conta.py
+++ conta.py
@@ -1,121 +1,137 @@
+
+
+
+
 class Historico:
-    def __init__(self):
-        self._deposito = []
-        self._saque = []
-        self._transferencia = []         
-    
-    @property
-    def get_deposito(self):
-        if self._deposito:
-            for x in self._deposito:
-                print(x)
-        else:
-            print('Sem histórico de depósitos') 
+   __slots__ = ['_deposito','_saque','_transferencia']
+   
+   def __init__(self):
+      self._deposito = []
+      self._saque = []
+      self._transferencia = []         
+   
+   @property
+   def get_deposito(self):
+      if self._deposito:
+         for x in self._deposito:
+            print(x)
+      else:
+         print('Sem histórico de depósitos') 
 
-    def deposito(self, d):
-        self._deposito.append(d)
+   def deposito(self, d):
+      self._deposito.append(d)
 
-    @property
-    def get_saque(self):
-        if self._saque:
-            for x in self._saque:
-                print(x)
-        else:
-            print('Sem histórico de saques') 
+   @property
+   def get_saque(self):
+      if self._saque:
+         for x in self._saque:
+            print(x)
+      else:
+         print('Sem histórico de saques') 
 
-    def saque(self, s):
-        self._saque.append(s)
+   def saque(self, s):
+      self._saque.append(s)
 
-    @property
-    def get_transferencia(self):
-        if self._transferencia:
-            print(self._transferencia)
-        else:
-            print('Sem histórico de transferncias') 
+   @property
+   def get_transferencia(self):
+      if self._transferencia:
+         print(self._transferencia)
+      else:
+         print('Sem histórico de transferncias') 
 
-    def transferencia(self, t):
-        self._transferencia.append(t)
-
+   def transferencia(self, t):
+      self._transferencia.append(t)
 
 
 class Conta:
-    _total_contas = 0
-    __slots__ = ['_titular','_numero','_saldo', '_historico']
+   _total_contas = 0
+   __slots__ = ['_titular','_numero','_saldo', '_historico']
 
-    def __init__(self, cliente, numero, saldo):         
-        self._titular = cliente 
-        self._numero = numero 
-        self._saldo = saldo 
-        self._historico = Historico()
-        Conta._total_contas += 1
+   def __init__(self, cliente, numero, saldo):         
+      self._titular = cliente 
+      self._numero = numero 
+      self._saldo = saldo 
+      self._historico = Historico()
+      Conta._total_contas += 1
 
-    @staticmethod
-    def get_total_contas():
-        return Conta._total_contas
+   @staticmethod
+   def get_total_contas():
+      return Conta._total_contas
 
-    @property
-    def get_numero(self):
-        return self._numero 
+   @property
+   def get_titular(self):
+      return self._titular.get_nome 
 
-    @property
-    def get_saldo(self):
-        return self._saldo 
+   @property
+   def get_numero(self):
+      return self._numero 
 
-    def saldo(self, saldo):
-        self._saldo = saldo
+   @property
+   def get_saldo(self):
+      return self._saldo 
 
+   def saldo(self, saldo):
+      self._saldo = saldo
+   
+   @property
+   def get_historico(self):
+      self._historico.get_deposito
+      self._historico.get_saque
+      self._historico.get_transferencia
 
-    def deposita(valor):
-        self._saldo += valor
-        self._historico.set_deposito(['Depósito de: ', valor]) 
-    
-    def saca(valor):
-        if self._saldo >= valor:
-            self._saldo -= valor
-            self._historico.set_saque(['Saque de: ', valor])
-    
-    def extrado(valor):
-        print(self._saldo)
-    
-    def transfere(self, conta, valor):
-        if self._saldo >= valor:
-            self._saldo -= valor
-            conta._saldo += valor
-            conta._historico.transferencia(['Valor recebido: ', valor, 'de: ', self._titular.get_nome])
-            self._historico.transferencia(['TEV de: ', valor, 'para: ', conta._titular.get_nome])
-        else:
-            print('\nSaldo insuficiente')
+   def deposita(self, valor):
+      self._saldo += valor
+      self._historico.deposito(['Depósito de: ', valor]) 
+   
+   def saca(self, valor):
+      if self._saldo >= valor:
+         self._saldo -= valor
+         self._historico.saque(['Saque de: ', valor])
+   
+   def extrato(self):
+      print(self._saldo)
+   
+   def transfere(self, conta, valor):
+      if self._saldo >= valor:
+         self._saldo -= valor
+         conta._saldo += valor
+         conta._historico.transferencia(['Valor recebido: ', valor, 'de: ', self._titular.get_nome])
+         self._historico.transferencia(['TEV de: ', valor, 'para: ', conta._titular.get_nome])
+      else:
+         print('\nSaldo insuficiente')
 
 
 class Cliente:
-    def __init__(self, nome, sobrenome, cpf): 
-        self._nome = nome 
-        self._sobrenome = sobrenome 
-        self._cpf = cpf 
+   __slots__ = ['_nome','_sobrenome','_cpf']
 
-    @property
-    def get_nome(self):
-        return self._nome 
+   def __init__(self, nome, sobrenome, cpf): 
+      self._nome = nome 
+      self._sobrenome = sobrenome 
+      self._cpf = cpf 
 
-    def nome(self, n):
-        self._nome = n
+   @property
+   def get_nome(self):
+      return self._nome 
 
-    @property
-    def get_sobrenome(self):
-        return self._sobrenome 
+   def nome(self, n):
+      self._nome = n
 
-    def sobrenome(self, s):
-        self._sobrenome = s
+   @property
+   def get_sobrenome(self):
+      return self._sobrenome 
 
-    @property
-    def get_cpf(self):
-        return self._cpf 
+   def sobrenome(self, s):
+      self._sobrenome = s
 
-    def cpf(self, c):
-        self._cpf = c
-    
-    
+   @property
+   def get_cpf(self):
+      return self._cpf 
 
-    
+   def cpf(self, c):
+      self._cpf = c
+   
+   
 
-    
+   
+
+   
--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from conta import Conta, Cliente
+from banco import Banco
 
-joao = Cliente('João','de Deus','606')
-maria = Cliente('maria','passa na frente','777') 
+caixa = Banco()
+caixa.menu()
 
-conta1 = Conta(joao,987,1.255)
-conta2 = Conta(maria,679,3.219)
 
-print(conta1._titular.get_nome,conta1.get_numero,conta1.get_saldo)
-print(conta2._titular.get_nome,conta2.get_numero,conta2.get_saldo)
 
-conta2.transfere(conta1,1.435)
-
-print('\n',conta1._titular.get_nome,conta1.get_numero,conta1.get_saldo)
-print(conta2._titular.get_nome,conta2.get_numero,conta2.get_saldo)
-
-print('\n')
-conta1._historico.get_transferencia
-conta2._historico.get_transferencia
