dc35a086929c98cc7154daf8add0b71544883fa8
cdanielss <cdanielss1973@outlook.com>
2021-05-05 15:33:30 -0300

    Criacao de telas e da main

---
+++ Interface/main.py
@@ -0,0 +1,58 @@
+import sys
+import os
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
+from PyQt5.QtCore import QCoreApplication
+
+from telaPrincipal import TelaPrincipal
+from telaCadastro import TelaCadastro
+from telaBanco import TelaBanco
+
+class Ui_Main(QtWidgets.QWidget):
+    def setupUi(self, Main):
+        Main.setObjectName("Main")
+        Main.resize(640, 480)
+        
+        self.QtStack = QtWidgets.QStackedLayout()
+        self.stack0 = QtWidgets.QMainWindow()
+        self.stack1 = QtWidgets.QMainWindow()
+        self.stack2 = QtWidgets.QMainWindow()
+        
+        self.tela_principal = TelaPrincipal()
+        self.tela_principal.setupUi(self.stack0)
+        self.tela_cadastro = TelaCadastro()
+        self.tela_cadastro.setupUi(self.stack1)
+        self.tela_banco = TelaBanco()
+        self.tela_banco.setupUi(self.stack2)
+
+        self.QtStack.addWidget(self.stack0)
+        self.QtStack.addWidget(self.stack1)
+        self.QtStack.addWidget(self.stack2)
+    
+class Main(QMainWindow, Ui_Main):
+    def __init__(self, parent = None):
+        super(Main, self).__init__(parent)
+        self.setupUi(self)
+
+        self.tela_principal.pushButton.clicked.connect(self.abrirTelaLogado)
+        self.tela_principal.pushButton_2.clicked.connect(self.abrirTelaCadastro)
+
+        self.tela_cadastro.pushButton_2.clicked.connect(self.botaoVoltarHome)
+
+        self.tela_banco.pushButton_6.clicked.connect(self.botaoVoltarHome)
+    
+    def botaoVoltarHome(self):
+        self.QtStack.setCurrentIndex(0)
+
+    def abrirTelaLogado(self):
+        self.QtStack.setCurrentIndex(2)
+
+    def abrirTelaCadastro(self):
+        self.QtStack.setCurrentIndex(1)
+        
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    show_main = Main()
+    sys.exit(app.exec_())
---
+++ Interface/telaPrincipal.py
@@ -0,0 +1,75 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
+from PyQt5.QtCore import QCoreApplication
+
+
+class TelaPrincipal(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(260, 20, 91, 41))
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(50, 120, 161, 21))
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(160, 190, 41, 16))
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(250, 120, 241, 25))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(250, 190, 241, 25))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(340, 290, 151, 41))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(110, 290, 151, 41))
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Login"))
+        self.label_2.setText(_translate("MainWindow", "Número da Conta"))
+        self.label_3.setText(_translate("MainWindow", "CPF"))
+        self.pushButton.setText(_translate("MainWindow", "Entrar"))
+        self.pushButton_2.setText(_translate("MainWindow", "Cadastra-se"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = TelaPrincipal()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
